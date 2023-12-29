8b667e9fc7b9516513832601da7a99afd065e7fb
keniwataro <keniwa30421@gmail.com>
2023-12-29 11:31:36 +0900

    認証機能追加、及び日本語化

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| ウェブ・ルート
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| 以下は、あなたのアプリケーションのためにウェブ・ルートを登録できる場所です。
+| これらのルートは、RouteServiceProvider によって読み込まれ、すべてが "web" 
+| ミドルウェアグループに割り当てられます。素晴らしいものを作りましょう！
 |
 */
 
+// ルートの場合はdashboard画面に移動（ログインしていない場合はログイン画面）
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// ログインユーザー認証用ルート
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+//  このファイル(web.php)があるディレクトリ(routes)内の
+//  auth.phpを読み込む処理
+require __DIR__.'/auth.php';
