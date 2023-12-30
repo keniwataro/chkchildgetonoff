6a3fdd6b2c07a831a84bbd7a8ca1c8252601fec9
keniwataro <keniwa30421@gmail.com>
2023-12-30 15:54:00 +0900

    必要なDBを作成しました！

---
+++ app/Http/Controllers/GetonbusController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Getonbus;
+use Illuminate\Http\Request;
+
+class GetonbusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Getonbus $getonbus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Getonbus $getonbus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Getonbus $getonbus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Getonbus $getonbus)
+    {
+        //
+    }
+}
---
+++ app/Models/Getonbus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Getonbus extends Model
+{
+    use HasFactory;
+}
