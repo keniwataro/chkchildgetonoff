c525d4df252ec25bb6d1cb38125ef2b113df2b72
S-Aaai <sato.hashimoto5@gmail.com>
2023-12-30 12:08:47 +0900

    button, frame 追加

--- resources/views/test-buttons.blade.php
+++ resources/views/test-buttons.blade.php
@@ -1,6 +1,19 @@
 <x-app-layout>
 <x-buttons.register-button>登録</x-buttons.register-button>
 <x-buttons.edit-button>編集</x-buttons.edit-button>
+<x-buttons.change-page-button>行き</x-buttons.change-page-button>
+<x-buttons.change-page-button>帰り</x-buttons.change-page-button>
+<x-buttons.next-button></x-buttons.next-button>
+
+
+
+<x-frame.name-frame>にしだ　あかり</x-frame.name-frame>
+<x-frame.etc-frame>2023/12/30</x-frame.etc-frame>
+<x-frame.etc-frame>A地点</x-frame.etc-frame>
+<x-frame.head-frame>なまえ</x-frame.head-frame>
+<x-frame.head-frame>誕生日</x-frame.head-frame>
+<x-frame.head-frame>送迎場所</x-frame.head-frame>
+
 
 <div class="w-30">
 <x-buttons.getoff-button getonoff="おりる！">
