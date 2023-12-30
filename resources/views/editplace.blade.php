7a97b191c5276644d0686b202dd733a2ac1a06e6
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2023-12-30 22:29:05 +0900

    editplaceの画面作成　途中経過

---
+++ resources/views/editplace.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+<header>
+    <div>送迎場所を編集</div>
+    <div>場所名</div><input type="text">
+    <x-buttons.register-button>登録</x-buttons.register-button>
+    
+    {{-- ファイルの読み込み部分 --}}
+    <div>ファイル読み込み</div>
+    <div>
+        <form action="#" method="post" enctype="multipart/form-data">
+            <label for="fileInput">ファイルを添付:</label>
+            <input type="file" id="fileInput" name="fileInput"> 
+            <input type="submit" value="読み込む">
+        </form>
+    </div>
+
+</header>
+1<x-frame.editplace-frame>test</x-frame.editplace-frame>
+
+
+</x-app-layout>
