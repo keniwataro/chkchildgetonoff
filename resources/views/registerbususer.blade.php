b18a564126493ce8df3040ae34328290a3199070
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2023-12-31 12:01:37 +0900

    バス利用画面　作成途中2

---
+++ resources/views/registerbususer.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+
+<table>
+    <tr>
+    <th><x-frame.head-frame>バス利用有無</x-frame.head-frame></th>    
+    <th><x-frame.head-frame>なまえ</x-frame.head-frame></th>
+    <th><x-frame.head-frame>誕生日</x-frame.head-frame></th>
+    <th><x-frame.head-frame>送迎場所</x-frame.head-frame></th>
+    <th><x-frame.head-frame>バス名</x-frame.head-frame></th>
+</tr>
+<tr>
+    <td><x-buttons.status-button>行き/帰り</x-buttons.status-button></td>
+    <td><x-frame.name-frame>にしだ　あかり</x-frame.name-frame></td>
+    <td><x-frame.etc-frame>2023/12/30</x-frame.etc-frame></td>
+    <td><x-frame.etc-frame>A地点</x-frame.etc-frame></td>
+    <td><x-frame.etc-frame>バス1</x-frame.etc-frame></td>
+</tr>
+</table>
+
+
+</x-app-layout>
