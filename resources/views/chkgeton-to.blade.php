5dfffb2f1ca4b73031c4ba7d023312d1a08ad264
S-Aaai <sato.hashimoto5@gmail.com>
2024-01-04 16:30:11 +0900

    9_chkgeton_to_園児側乗降車確認画面 作成

---
+++ resources/views/chkgeton-to.blade.php
@@ -0,0 +1,19 @@
+<x-layout.mobile>
+
+    <header class="flex justify-around pt-4 items-center text-center text-3xl font-bold">
+        <x-buttons.back-button ></x-buttons.back-button>
+            １／５
+        <x-buttons.next-button></x-buttons.next-button>
+    </header></br>
+
+    <div class="w-30 text-center">
+        <x-buttons.geton-button getonoff="のる！">
+            やあ！
+        </x-buttons.geton-button>
+    </div>
+
+    <div class="absolute inset-x-0 bottom-0 h-36 text-center">
+        <div class="text-3xl font-bold">にしだ　あかり<span class="text-lg">さん</span></div>
+    </div>
+
+</x-layout.mobile>
