fcd3f52eb8ed82a82db67f21af68422ecff8c8c7
S-Aaai <sato.hashimoto5@gmail.com>
2024-01-04 16:01:47 +0900

    11. chkgeton-to-last画面　作成

---
+++ resources/views/chkgeton-to-last.blade.php
@@ -0,0 +1,34 @@
+<x-layout.mobile>
+
+    <header class="flex justify-start pt-4 pl-4">
+        <x-buttons.back-button ></x-buttons.back-button>
+    </header></br></br></br>
+
+    <div class="h-36 text-center">
+        <div class="text-3xl font-bold">みんなのったよ！</div>
+    </div>
+
+    <button class="mx-auto btn mb-4 font-bold text-3xl text-white">
+            次の場所へ行く
+    </button>
+    <button class="mx-auto btn font-bold text-3xl text-white">
+        一覧表示
+    </button>
+
+
+    <style>
+        .btn {
+        z-index: 1;
+        cursor: pointer;
+        width: 240px;
+        height: 100px;
+        background: #ce89c8;
+        border-radius: 16px;
+        box-shadow: 0 10px 0 rgba(206, 137, 200, 0.2);
+        transition: all 100ms;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        }
+    </style>
+</x-layout.mobile>
